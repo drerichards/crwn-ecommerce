23da36e7998daf3cdd9bb73556b9aa1fe781eede
Andre Richardson <andrerichards@Sridevi-PC.localdomain>
2019-09-18 11:46:02 -0400

    first commit

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/homepage.scss';
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">Jackets</div>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
