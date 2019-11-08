82db398a70b935c9ccc4dca0857cfec0ebf5e734
A Richardson <g.andrerichards@gmail.com>
2019-11-08 04:50:56 -0500

    update nav to withRouter

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-<<<<<<< HEAD
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
 );
->>>>>>> 85572ccfc8cbed52ee80e08641356991bf18a83c
