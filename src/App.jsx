3f98f4afeb4718f20526535c05ce06ce745e3c08
A Richardson <g.andrerichards@gmail.com>
2019-11-26 23:21:22 -0500

    add sign out

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Header from './components/header/Header';
 import HomePage from './components/pages/home/HomePage';
@@ -6,17 +6,42 @@ import ShopPage from './components/pages/shop/ShopPage';
 import AuthenticationPage from './components/pages/authentication/AuthenticationPage';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/sign-in' component={AuthenticationPage} />
-      </Switch>
-    </div>
-  );
+import {auth} from './firebase/firebase.utils'
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user})
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render (){
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/sign-in' component={AuthenticationPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.scss'
 
-const Header = () => {
+import {auth} from '../../firebase/firebase.utils'
+
+const Header = ({currentUser}) => {
     return (
         <div className="header">
             <Link to="/" className="logo-container">
@@ -12,7 +14,11 @@ const Header = () => {
             <div className="nav-links">
                 <Link to="/shop" className="link">Shop</Link>
                 <Link to="/" className="link">Contact</Link>
-                <Link to="/sign-in" className="link">Sign In</Link>
+                {
+                    currentUser ?
+                    <div className='option link' onClick={() => auth.signOut()}>Sign Out</div> :
+                    <Link to="/sign-in" className="option link">Sign In</Link>
+                }
             </div>
         </div>
     )
