1765d725c18c99c156386675c9371f61d48f0ab8
A Richardson <g.andrerichards@gmail.com>
2019-11-26 22:57:43 -0500

    hide keys

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,17 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-
-const config = {
-    apiKey: "AIzaSyCaJCpCWfWU9IhDZdSBjADHTr0ETH4lq1k",
-    authDomain: "crwn-db-b7fb2.firebaseapp.com",
-    databaseURL: "https://crwn-db-b7fb2.firebaseio.com",
-    projectId: "crwn-db-b7fb2",
-    storageBucket: "crwn-db-b7fb2.appspot.com",
-    messagingSenderId: "476926460841",
-    appId: "1:476926460841:web:bc9fda3ab7c0b02561bab8",
-    measurementId: "G-PC08B9MLBT"
-}
+import {config} from '../keys'
 
 firebase.initializeApp(config)
 
