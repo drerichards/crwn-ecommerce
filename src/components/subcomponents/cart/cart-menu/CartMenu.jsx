0a46fe9b919f89d46c067d7a3c2db009966102a1
A Richardson <g.andrerichards@gmail.com>
2019-11-28 15:50:21 -0500

    create cart menu and items

--- src/components/subcomponents/cart/cart-menu/CartMenu.jsx
+++ src/components/subcomponents/cart/cart-menu/CartMenu.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import {connect} from 'react-redux'
+
+import CartItem from '../cart-item/CartItem'
 import Button from '../../../library/button/Button'
 import './cart-menu.scss'
 
-const CartMenu = () => {
+const CartMenu = ({cartItems}) => {
     return (
         <div className='cart-menu'>
-            <div className="cart-items"></div>
+            <div className="cart-items">
+                {
+                    cartItems.map(cartItem => (
+                        <CartItem key={cartItem.id} item={cartItem} />
+                    ))
+                }
+            </div>
             <Button>Go To Checkout</Button>
         </div>
     )
 }
 
-export default CartMenu
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems
+})
+export default connect(mapStateToProps)(CartMenu)
