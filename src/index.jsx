1bf75a0d573ad61ac43b417bf3dae130ca69dc3e
A Richardson <g.andrerichards@gmail.com>
2019-11-30 01:35:03 -0500

    put collections in sep comp

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,9 @@ import { store, persistor } from './redux/store'
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
+            <PersistGate persistor={persistor}>
                 <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>,
     document.getElementById('root')
--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import CollectionsPage from '../../components/pages/shop-page/collections/CollectionsPreview'
+import { Route } from 'react-router-dom'
 
-export const ShopPage = ({ collections }) => {
+import CollectionsPreview from '../../components/pages/shop-page/collections/CollectionsPreview'
+import CollectionPage from '../collection/CollectionPage'
+
+export const ShopPage = ({ match }) => {
     return (
         <div className="shop-page">
-            <CollectionsPage />
+            <Route exact path={`${match.path}`} component={CollectionsPreview} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
         </div>
     )
 }
