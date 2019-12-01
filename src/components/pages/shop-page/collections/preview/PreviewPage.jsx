558bfb9ae063bfc8e57fc053d70a41a1ac19da31
A Richardson <g.andrerichards@gmail.com>
2019-11-30 23:48:17 -0500

    add method to convert obj to arr for coll page

--- src/components/pages/shop-page/collections/preview/PreviewPage.jsx
+++ src/components/pages/shop-page/collections/preview/PreviewPage.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import PreviewItem from './preview-item/PreviewItem'
 import './preview-page.scss'
 
-const PreviewPage = ({ title, items }) => (
-    <div className="preview-page">
-        <h1 className="title">{title.toUpperCase()}</h1>
+const PreviewPage = (props) => {
+    console.log(props)
+    return (<div className="preview-page">
         <div className="preview">
             {
-                items
-                    .filter((item, idx) => idx < 4)
-                    .map(item => {
-                        return <PreviewItem key={item.id} item={item} />
-                    })
+                // items
+                //     .filter((item, idx) => idx < 4)
+                //     .map(item => (
+                //         <PreviewItem key={item.id} item={item} />
+                //     )
+                // )
             }
         </div>
-    </div>
-)
+    </div>)
+}
 
 export default PreviewPage;
