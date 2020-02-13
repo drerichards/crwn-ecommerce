2a353040ea8bd05aec24f02ad11d7a4070d60a4c
A Richardson <g.andrerichards@gmail.com>
2020-02-12 19:36:21 -0500

    containers

---
+++ src/components/pages/shop-page/collections/CollectionsPreviewContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+
+import { selectCollectionsForPreview } from '../../../../redux/shop/shopSelectors'
+
+const CollectionsPreviewContainer = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CollectionsPreviewContainer
